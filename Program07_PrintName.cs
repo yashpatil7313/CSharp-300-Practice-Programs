84ad38b0e69c112d250e2c5766b535422338571e
Yash patil <yashrspatil7313@gmail.com>
2026-01-22 21:14:46 +0530

    Create Program07_PrintName.cs

---
+++ Program07_PrintName.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 7;
+
+        if (num % 2 == 0)
+            Console.WriteLine("Even Number");
+        else
+            Console.WriteLine("Odd Number");
+    }
+}
