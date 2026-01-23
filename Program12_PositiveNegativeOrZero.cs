bfcedd7557d1a047afa8ca5e7ee0896f300d666a
Yash patil <yashrspatil7313@gmail.com>
2026-01-23 09:15:13 +0530

    Create Program12_PositiveNegativeOrZero.cs

---
+++ Program12_PositiveNegativeOrZero.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 0;
+
+        if (num > 0)
+            Console.WriteLine("Positive Number");
+        else if (num < 0)
+            Console.WriteLine("Negative Number");
+        else
+            Console.WriteLine("Zero");
+    }
+}
