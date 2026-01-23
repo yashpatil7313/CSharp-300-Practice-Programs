4694118ec26228ac8b2e4e74bc2f0ff67cb39c82
Yash patil <yashrspatil7313@gmail.com>
2026-01-23 09:18:45 +0530

    Create Program13_LargestOfTwoNumbers.cs

---
+++ Program13_LargestOfTwoNumbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 15, b = 25;
+
+        if (a > b)
+            Console.WriteLine("A is greater");
+        else
+            Console.WriteLine("B is greater");
+    }
