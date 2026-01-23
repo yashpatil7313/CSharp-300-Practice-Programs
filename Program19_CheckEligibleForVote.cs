0b1784ac288fa14df91787215e2ae2b52e86aa3d
Yash patil <yashrspatil7313@gmail.com>
2026-01-23 09:23:26 +0530

    Create Program19_CheckEligibleForVote.cs

---
+++ Program19_CheckEligibleForVote.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 20;
+
+        if (age >= 18)
+            Console.WriteLine("Eligible for Voting");
+        else
+            Console.WriteLine("Not Eligible for Voting");
+    }
+}
