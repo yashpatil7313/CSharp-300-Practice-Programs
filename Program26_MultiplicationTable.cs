eb2a1cf6747637608e5030b28a298094add9a441
Yash patil <yashrspatil7313@gmail.com>
2026-01-24 10:24:29 +0530

    Create Program26_MultiplicationTable.cs

---
+++ Program26_MultiplicationTable.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 5;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
