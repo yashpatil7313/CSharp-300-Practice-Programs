cadf7ffb8bc9bfc35b33ea47e0aba8ba4970aa52
Yash patil <yashrspatil7313@gmail.com>
2026-01-25 09:29:11 +0530

    Create Program31_PrintArrayElements.cs

---
+++ Program31_PrintArrayElements.cs
@@ -0,0 +1,17 @@
+using System; // Import System namespace
+
+class Program
+{
+    static void Main()
+    {
+        // Declare and initialize an integer array
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        // Loop through each element in the array
+        foreach (int i in arr)
+        {
+            // Print each element
+            Console.WriteLine(i);
+        }
+    }
+}
