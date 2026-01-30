7b2b3a8cd3a4c55534a75efc8e3857b47f578d13
Yash patil <yashrspatil7313@gmail.com>
2026-01-30 21:53:10 +0530

    Create Program46_ConcatenateTwoStrings.cs

---
+++ Program46_ConcatenateTwoStrings.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare two strings
+        string str1 = "Hello";
+        string str2 = "World";
+
+        // Concatenate using +
+        string result = str1 + " " + str2;
+
+        Console.WriteLine("Concatenated String = " + result);
+    }
+}
