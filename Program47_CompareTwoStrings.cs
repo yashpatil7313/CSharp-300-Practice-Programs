6066c8a773a4d87686874c5a509c30a2f8b2e746
Yash patil <yashrspatil7313@gmail.com>
2026-01-30 21:53:33 +0530

    Create Program47_CompareTwoStrings.cs

---
+++ Program47_CompareTwoStrings.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare two strings
+        string str1 = "apple";
+        string str2 = "apple";
+
+        // Compare strings
+        if (str1 == str2)
+            Console.WriteLine("Strings are equal");
+        else
+            Console.WriteLine("Strings are not equal");
+    }
+}
