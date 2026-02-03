17fb92302f6685e4a2fd68c45e7c378d5573d1ec
Yash patil <yashrspatil7313@gmail.com>
2026-02-03 11:04:54 +0530

    Create Program50_PrintNumbers1To10.cs

---
+++ Program50_PrintNumbers1To10.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program50_PrintNumbers1To10
+{
+    static void Main()
+    {
+        // Loop from 1 to 10
+        for (int i = 1; i <= 10; i++)
+        {
+            // Print each number
+            Console.WriteLine(i);
+        }
+    }
+}
