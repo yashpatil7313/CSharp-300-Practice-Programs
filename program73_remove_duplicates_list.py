e0edbc071557975e2bf19ddfaf0cbb14420b6b1d
Yash patil <yashrspatil7313@gmail.com>
2026-02-19 09:21:53 +0530

    Create program73_remove_duplicates_list.py

---
+++ program73_remove_duplicates_list.py
@@ -0,0 +1,8 @@
+# Program 73: Remove duplicates from a list
+
+numbers = [10, 20, 20, 30, 40, 40, 50]
+
+unique_numbers = list(set(numbers))
+
+print("List after removing duplicates:", unique_numbers)
+
