0e814486b027e95fdae90a8f5838240622a4b553
Yash patil <yashrspatil7313@gmail.com>
2026-02-19 09:33:20 +0530

    Create program75_check_perfect_number.py

---
+++ program75_check_perfect_number.py
@@ -0,0 +1,13 @@
+# Program 75: Check perfect number
+
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, num):
+    if num % i == 0:
+        sum += i
+
+if sum == num:
+    print("Perfect number")
+else:
+    print("Not a perfect number")
