57a56288cea3c1cbeb7aa04bc11973f0a4fa1fd0
Yash patil <yashrspatil7313@gmail.com>
2026-02-21 12:37:45 +0530

    Create Program75_SortArrayAscending.cs

---
+++ Program75_SortArrayAscending.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Program75_SortArrayAscending
+{
+    static void Main()
+    {
+        int[] numbers = { 5, 2, 8, 1, 3 };
+
+        // Bubble Sort (Ascending)
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            for (int j = 0; j < numbers.Length - i - 1; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Sorted Array (Ascending):");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    }
+}
